d714fcd15a006dc779e06f42bcfc6c589b9217e9
Devineni <mail2devineni1217@gmail.com>
2021-09-23 22:07:31 +0530

    add arrays in the string's

--- Takung input/raw_input.py
+++ Takung input/raw_input.py
@@ -3,5 +3,5 @@ raw_input ( ) : This function works in older version (like Python 2.x).
 This function takes exactly what is typed from the keyboard, 
 convert it to string and then return it to the variable in which we want to store. 
 For example –'''
-name = raw_input("enter your name:")
-print (name)
+place = raw_input("Where do you stay?")
+print ("my palce is", place)
--- variables/Local_Variables.py
+++ variables/Local_Variables.py
@@ -4,3 +4,8 @@ def fun_name():
    name="Devineni is local variable"
    print(name)
 fun_name()
+#when we call the function, the control is transferred to the function where the variable named 'name' gets initialized and printed.
+#As soon as the function exits and passes the control back to the main program, the variable 'name' is no longer in scope.
+#This is how a local variable behaves, and is scope lies in local to the piece of code where it’s defined
+'''What will happen if we try to print the variable 'name' outside of the function?'''
+#ans) name is not define
